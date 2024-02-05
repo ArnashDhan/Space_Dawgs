b6c59d926f707ee72167c429f6397fd8f500fd97
ubermensch04 <112368199+ubermensch04@users.noreply.github.com>
2024-02-05 16:50:58 +0530

    Add files via upload

---
+++ space-dawgsv1/main/background.go
@@ -0,0 +1,27 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/background.atlas\"\n"
+  "default_animation: \"Space Background\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "size {\n"
+  "  x: 1920.0\n"
+  "  y: 1080.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
